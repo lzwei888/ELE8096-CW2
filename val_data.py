cd6d6d0bf2a40f0431e3cc5f43e4133677d67cf3
lzwei888 <l18519655819@gmail.com>
2024-02-22 15:14:35 +0000

    data processing

--- val_data.py
+++ val_data.py
@@ -1,7 +1,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits import mplot3d
 from sklearn.linear_model import LinearRegression
 import data
+import pandas as pd
 
 data_array = data.data_array
 # get data, in array
@@ -13,68 +15,47 @@ tem = data_array[4]
 hum = data_array[5]
 
 
-# find locations of elements in array that greater than threshold value
-def outlier_finder(data, threshold):
+# function to define the outliers, which is the abs(residual) > std(residual) * 2
+def outlier_finder(arr1):
     outlier_pos = []
-    for i in range(len(data)):
-        if data[i] > threshold:
+    arr1 = np.sort(arr1)
+    x = np.array([i for i in range(0, len(arr1))])
+    fit1 = np.polyfit(x.astype(np.float64), arr1.astype(np.float64), 1)
+    poly = np.poly1d(fit1)
+    pred = poly(x)
+    residual = arr1 - pred
+    std = 2 * np.std(residual)
+    for i in range(len(residual)):
+        if np.abs(residual[i]) >= 2 * std:
             outlier_pos.append(i)
     return outlier_pos
 
-
-# remove values in the 2 array at the same time
+# Remove values in the 2 array at the same time
 def outlier_remover(data1, data2, outlier_pos):
-    index = np.flip(outlier_pos)
-    for i in index:
-        data1 = np.delete(data1, i)
-        data2 = np.delete(data2, i)
+    n = len(outlier_pos)
+    max_indices = np.argsort(data1)[-n:]
+    data1 = np.delete(data1, max_indices)
+    data2 = np.delete(data2, max_indices)
+    return data1, data2
+
+# Remove outliers as groups, in two related variables
+def group_remover(data1, data2):
+    data1, data2 = outlier_remover(data1, data2, outlier_finder(data1))
+    data2, data1 = outlier_remover(data2, data1, outlier_finder(data2))
     return data1, data2
 
 
-# # testing
-# data1 = np.array([1, 2, 3, 4, 5, 6, 7])  # define the 1st array
-# data2 = np.array([0, 4, 5, 6, 7, 8, 9])  # define the 2nd array
-# outlier_pos = outlier_finder(data1, 4)  # find the pos of elements in 2 array that greater than 4
-# data1, data2 = outlier_remover(data1, data2, outlier_pos)  # remove elements in 2 array that greater than 4
-# print("array 1: ", data1, "\narray 2: ", data2, "\nelements at position", outlier_pos, "is removed")  # show results
+# example: remove outliers in NO2 and O3, and store the result in Excel
+no2, o3 = group_remover(no2, o3)
+# data_df = pd.DataFrame(no2, o3)
+# writer = pd.ExcelWriter('test.xlsx')
+# data_df.to_excel(writer, 'page_1', float_format='%.5f')
+# writer.save()
 
-
-tem_model = tem[0:300]
-tem_test = tem[300:452]
-
-pm25_model = pm25[0:300]
-pm25_test = pm25[300:452]
-
-
-
-
-# polyfit
-tem_model = tem_model.astype(np.float64)
-pm25_model = pm25_model.astype(np.float64)
-
-z1=np.polyfit(tem_model,pm25_model,3)
-p1 = np.poly1d(z1)
-pm25_pred = p1(tem_model)
-print(z1)
-
-
-
-
-
-
-
-
-
-# # LinearRegression
-# tem_model=tem[0:300].reshape(-1, 1)
-# tem_test=tem[300:452].reshape(-1, 1)
-# # fit linear regression
-# tem_pm25_model = LinearRegression().fit(tem_model, pm25_model)
-# # print(tem_pm25_model.score(tem_model, pm25_model))
-# pm25_pred=tem_pm25_model.predict(tem_model)
-# # print(pm25_pred-pm25_model)
 
 # plot
-plt.scatter(tem_model,pm25_model)
-plt.scatter(tem_model,pm25_pred)
+plt.scatter(no2, o3)
+plt.xlabel("NO2")
+plt.ylabel("O3")
+plt.title("NO2-O3 (After removing outliers)")
 plt.show()
